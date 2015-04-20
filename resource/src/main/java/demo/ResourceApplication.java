afdeb285722eb539028ffcf0b6cf177b29b22b65
Mick Dudley <Mick@dudley.uk.com>
2015-04-20 13:43:15 +0100

    Tidy up, remove unused code

--- resource/src/main/java/demo/ResourceApplication.java
+++ resource/src/main/java/demo/ResourceApplication.java
@@ -4,10 +4,6 @@ package demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.security.oauth2.resource.EnableOAuth2Resource;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +17,7 @@ import java.util.UUID;
 public class ResourceApplication {
     @RequestMapping("/")
     public Map<String, String> home() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         Map<String, String> resp = new HashMap();
         resp.put("id", UUID.randomUUID().toString());
